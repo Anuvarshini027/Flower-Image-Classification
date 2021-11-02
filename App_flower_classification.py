b0c4c18dd927349f4908854e8f83fef0b65f8f39
Anuvarshini <60288450+Anuvarshini027@users.noreply.github.com>
2021-11-02 15:19:44 +0530

    Update App_flower_classification.py

--- App_flower_classification.py
+++ App_flower_classification.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import warnings
 warnings.filterwarnings("ignore") 
+
 import streamlit as st
 import time
 import wikipedia as wiki
@@ -11,7 +12,7 @@ from rake_nltk import Rake
 from keras.models import load_model
 from PIL import Image, ImageOps
 from keras.preprocessing.image import ImageDataGenerator
-
+from streamlit import caching
 
 
 st.title('Image Classification - Flower Dataset Project')
@@ -144,11 +145,9 @@ labels=['alpine sea holly',
  'wild rose',
  'windflower',
  'yellow iris']
-
-st.write(f"There are a total of {len(labels)} classes in the training dataset") 
 st.success("Loaded the Model :)")
 
-
+#option = st.multiselect('Would you like to choose random image from test dataset or upload image?',["Select Random Image from Test Dataset", "Upload from test dataset"])
 
 file = st.file_uploader("Choose an image...", type="jpeg")
 if file is not None:
@@ -171,9 +170,8 @@ if file is not None:
             st.write("Key: " + str(key))
             st.subheader("Here is some information about the predicted flower:)")
             try:
-                page = wiki.page(key)
-                st.info(page.summary)
-                st.success("Thank You :)")      
+                information = wiki.summary(key, 5)
+                st.info(information)
             except:
                 topics = wiki.search(key)
                 st.write(f"{labels[prediction].upper()} may refer to: ")
@@ -181,9 +179,14 @@ if file is not None:
                     st.write(i, topic)
                 choice = st.text_input("Enter a choice: ")
                 assert int(choice) in xrange(len(topics))
-                st.info(wiki.summary(topics[choice])
-                st.success("Thank You :)")      
-                
+                st.info(wiki.summary(topics[choice]))
+            #information = wiki.summary(labels[prediction], 2)
+            #st.subheader("Here is a little info about the flower:)")
+            #st.info(information)
+            
+    st.subheader("Thank You :)")
+            
+                    
 else:
     st.warning("No file has been chosen yet")
 
